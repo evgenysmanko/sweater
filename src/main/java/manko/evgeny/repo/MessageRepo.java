6385c468c7fc731060a5847b21ce94ef992c9dbc
Evgeny Manko <evsmanko@gmail.com>
2020-04-25 18:42:16 +0300

    Database

---
+++ src/main/java/manko/evgeny/repo/MessageRepo.java
@@ -0,0 +1,10 @@
+package manko.evgeny.repo;
+
+import manko.evgeny.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
