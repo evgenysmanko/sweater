f056f6be5db9368870bec4146fbf53fe147b886a
Evgeny Manko <evsmanko@gmail.com>
2020-04-28 13:41:04 +0300

    Database

---
+++ src/main/java/manko/evgeny/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package manko.evgeny.repo;
+
+import manko.evgeny.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
